1c99773866684cc68cc7947e31c356bdf96277cc
Valery <bananchik97@gmail.com>
2019-02-18 22:18:39 +0200

    Finish main page

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -13,6 +13,10 @@ $this->registerCssFile('@web/css/loader.css');
 
 <h2>Проект Random Seed</h2>
 
+<div class="welcomeMessage">
+	Пожалуйста, напишите ниже свою последовательность из случайных цифр от 0 до 9, не короче 300 символов, и получите оценку того, насколько она случайна. Если вы не понимаете, что происходит и зачем это все нужно, посетите раздел W.T.F. 
+</div>
+
 <?php $form = ActiveForm::begin(); ?>
 
 <?= $form->field($model, 'value')->textarea(['rows' => 15, 'id' => 'textSequence', 'type' => 'number' ]) ?>
